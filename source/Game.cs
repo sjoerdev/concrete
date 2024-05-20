fcad9826e330e017b9ac22bebd5f227e211554df
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-05-20 22:11:21 +0200

    initial silk window and opengl

--- source/Game.cs
+++ source/Game.cs
@@ -1,4 +1,8 @@
 using System;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+using Silk.NET.OpenGL;
 
 namespace Project;
 
@@ -7,37 +11,56 @@ class Entry
     static void Main()
     {
         Console.WriteLine("entry found");
-        // Game game = new Game();
-        // game.Start();
-        // game.update
+        new Game();
     }
 }
 
 class Game
 {
+    private static IWindow window;
+    private static GL opengl;
+
     public Scene activeScene = null;
     public List<Scene> scenes = [];
 
+    public Game()
+    {
+        var options = WindowOptions.Default;
+        options.Size = new Vector2D<int>(1280, 720);
+        options.Title = "untitled";
+        window = Window.Create(options);
+        
+        window.Load += Start;
+        window.Render += Update;
+        window.FramebufferResize += Resize;
+
+        window.Run();
+        window.Dispose();
+    }
+
     public void Start()
     {
-        LoadScene("scene.xml");
-        PlayScene(0);
+        opengl = GL.GetApi(window);
+        // LoadScene("scene.xml");
     }
 
-    public void Update(float deltaTime)
+    public void Update(double deltaTime)
     {
-        if (activeScene != null) activeScene.Update(deltaTime);
+        if (activeScene != null) activeScene.Update((float)deltaTime);
+        opengl.Clear(ClearBufferMask.ColorBufferBit);
+        opengl.ClearColor(System.Drawing.Color.CornflowerBlue);
     }
 
-    private void PlayScene(int index)
+    public void Resize(Vector2D<int> size)
     {
-        activeScene = scenes[index];
-        activeScene.Start();
+        opengl.Viewport(size);
     }
 
     private void LoadScene(string path)
     {
         var scene = new Scene(path);
         scenes.Add(scene);
+        activeScene = scene;
+        activeScene.Start();
     }
 }
