6ccdd14cbea17285d5951979d49b65767b287f50
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-05-22 22:40:21 +0200

    extra vertex data

--- source/MeshRenderer.cs
+++ source/MeshRenderer.cs
@@ -1,6 +1,7 @@
 using System;
 using Silk.NET.OpenGL;
 using Silk.NET.Assimp;
+using System.Numerics;
 
 namespace Project;
 
@@ -10,7 +11,7 @@ public unsafe class MeshRenderer : Component
 
     public Shader shader;
     public string modelPath;
-    uint vertexCount;
+    public uint indicesCount;
     uint vao;
 
     public MeshRenderer(string modelPath)
@@ -37,45 +38,88 @@ public unsafe class MeshRenderer : Component
         shader.SetMatrix4("proj", Camera.main.proj);
         
         opengl.BindVertexArray(vao);
-        opengl.DrawArrays(GLEnum.Triangles, 0, vertexCount / 3);
+        opengl.DrawArrays(GLEnum.Triangles, 0, indicesCount / 3);
         opengl.BindVertexArray(0);
     }
 
-    uint VertexArrayFromModel(string path)
+    private uint VertexArrayFromModel(string modelPath)
     {
-        float[] vertices = LoadModel(path);
-        vertexCount = (uint)vertices.Length;
-        uint vertexArray = opengl.GenVertexArray();
-        opengl.BindVertexArray(vertexArray);
-        uint vertexBuffer = opengl.GenBuffer();
-        opengl.BindBuffer(GLEnum.ArrayBuffer, vertexBuffer);
-        fixed (void* ptr = vertices) opengl.BufferData(GLEnum.ArrayBuffer, (nuint)(vertices.Length * sizeof(float)), ptr, GLEnum.StaticDraw);
-        opengl.VertexAttribPointer(0, 3, GLEnum.Float, false, 3 * sizeof(float), null);
+        LoadModel(modelPath, out Vertex[] vertices, out uint[] indices);
+
+        indicesCount = (uint)indices.Length;
+
+        List<float> vertbufferlist = [];
+        foreach (var vertex in vertices)
+        {
+            vertbufferlist.Add(vertex.position.X);
+            vertbufferlist.Add(vertex.position.Y);
+            vertbufferlist.Add(vertex.position.Z);
+        }
+        float[] vertbuffer = vertbufferlist.ToArray();
+
+        // generate vao, vbo, ebo
+        uint temp_vao = opengl.GenVertexArray();
+        uint temp_vbo = opengl.GenBuffer();
+        uint temp_ebo = opengl.GenBuffer();
+
+        // bind vao, vbo, ebo
+        opengl.BindVertexArray(temp_vao);
+        opengl.BindBuffer(GLEnum.ArrayBuffer, temp_vbo);
+        opengl.BindBuffer(GLEnum.ElementArrayBuffer, temp_ebo);
+
+        // set buffers
+        fixed (void* ptr = &vertbuffer[0]) opengl.BufferData(GLEnum.ArrayBuffer, (uint)(vertbuffer.Length * sizeof(float)), ptr, GLEnum.StaticDraw);
+        fixed (void* ptr = &indices[0]) opengl.BufferData(GLEnum.ElementArrayBuffer, (uint)(indices.Length * sizeof(uint)), ptr, GLEnum.StaticDraw);
+        
+        // atribute arrays
         opengl.EnableVertexAttribArray(0);
+        opengl.VertexAttribPointer(0, 3, GLEnum.Float, false, 3 * sizeof(float), null);
+        
+        // unbind
         opengl.BindBuffer(GLEnum.ArrayBuffer, 0);
+        opengl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
         opengl.BindVertexArray(0);
-        return vertexArray;
+
+        return temp_vao;
     }
 
-    private float[] LoadModel(string path)
+    private void LoadModel(string path, out Vertex[] vertices, out uint[] indices)
     {
         var assimp = Assimp.GetApi();
         var scene = assimp.ImportFile(path, (uint)PostProcessSteps.Triangulate | (uint)PostProcessSteps.JoinIdenticalVertices);
-        if (scene == null || scene->MRootNode == null) throw new Exception("Error loading model.");
+        if (scene == null) throw new Exception("Error loading model.");
 
-        var vertices = new List<float>();
+        var tempVertices = new List<Vertex>();
+        var tempIndices = new List<uint>();
         for (uint i = 0; i < scene->MNumMeshes; i++)
         {
             var mesh = scene->MMeshes[i];
             for (uint j = 0; j < mesh->MNumVertices; j++)
             {
-                var vertex = mesh->MVertices[j];
-                vertices.Add(vertex.X);
-                vertices.Add(vertex.Y);
-                vertices.Add(vertex.Z);
+                var position = mesh->MVertices[j];
+                var normal = mesh->MNormals[j];
+                var uv = Vector2.Zero;
+                if (mesh->MTextureCoords[0] != null) uv = new Vector2(mesh->MTextureCoords[0][j].X, mesh->MTextureCoords[0][j].Y);
+                tempVertices.Add(new Vertex(position, normal, uv));
+            }
+            for (uint j = 0; j < mesh->MNumFaces; j++)
+            {
+                var face = mesh->MFaces[j];
+                for (uint k = 0; k < face.MNumIndices; k++)
+                {
+                    tempIndices.Add(face.MIndices[k]);
+                }
             }
         }
         assimp.ReleaseImport(scene);
-        return vertices.ToArray();
+        vertices = tempVertices.ToArray();
+        indices = tempIndices.ToArray();
+    }
+    
+    private struct Vertex(Vector3 position, Vector3 normal, Vector2 uv)
+    {
+        public Vector3 position = position;
+        public Vector3 normal = normal;
+        public Vector2 uv = uv;
     }
 }
