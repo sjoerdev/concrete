bb25419f7372a883ebbd662ce86b873a62719ee3
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-08-28 15:36:24 +0200

    update meshrenderer

--- src/components/MeshRenderer.cs
+++ src/components/MeshRenderer.cs
@@ -1,4 +1,7 @@
 using System.Numerics;
+using SharpGLTF.Schema2;
+using SharpGLTF.Runtime;
+using SharpGLTF.Transforms;
 
 namespace Concrete;
 
@@ -6,6 +9,9 @@ public class MeshRenderer : Component
 {
     private Mesh[] meshes = [];
     private Shader shader = Shader.Default;
+
+    private bool animated = false;
+    private SceneInstance instance;
 
     [Include]
     public string modelPath
@@ -15,6 +21,8 @@ public class MeshRenderer : Component
         {
             currentModelPath = value;
             meshes = Extractor.GetMeshes(currentModelPath);
+            instance = SceneTemplate.Create(ModelRoot.Load(currentModelPath).DefaultScene).CreateInstance();
+            if (instance.GetDrawableInstance(0).Transform is SkinnedTransform) animated = true;
         }
     }
     private string currentModelPath;
@@ -28,19 +36,24 @@ public class MeshRenderer : Component
         shader.SetMatrix4("proj", perspective.proj);
         
         shader.SetLights(SceneManager.loadedScene.FindActiveLights());
+
+        if (animated)
+        {
+            var skinnedTransform = (SkinnedTransform)instance.GetDrawableInstance(0).Transform;
+            Matrix4x4[] skinnedMatrices = skinnedTransform.SkinMatrices.ToArray();
+            for (int i = 0; i < 100; i++) shader.SetMatrix4($"jointMatrices[{i}]", Matrix4x4.Identity);
+            for (int i = 0; i < skinnedMatrices.Length; i++) shader.SetMatrix4($"jointMatrices[{i}]", skinnedMatrices[i]);
+        }
         
         foreach (var mesh in meshes)
         {
             shader.SetVector4("matColor", mesh.material.color);
-
             var hasAlbedo = mesh.material.albedoTexture != null;
             shader.SetBool("matHasAlbedoTexture", hasAlbedo);
             if (hasAlbedo) shader.SetTexture("matAlbedoTexture", (uint)mesh.material.albedoTexture, 2);
-
             var hasRoughness = mesh.material.roughnessTexture != null;
             shader.SetBool("matHasRoughnessTexture", hasRoughness);
             if (hasRoughness) shader.SetTexture("matRoughnessTexture", (uint)mesh.material.roughnessTexture, 3);
-
             mesh.Render();
         }
     }
