d1d0d386a6aa74884adc677f26ef4f86f068d639
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-08-28 15:26:21 +0200

    update shader

--- res/shaders/default-vert.glsl
+++ res/shaders/default-vert.glsl
@@ -3,6 +3,8 @@
 layout (location = 0) in vec3 aPosition;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aUv;
+layout (location = 3) in vec4 joints;
+layout (location = 4) in vec4 weights;
 
 out vec2 uv;
 out vec3 normal;
@@ -13,11 +15,28 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
 
+uniform mat4 jointMatrices[100];
+
 void main()
 {
+    vec4 animatedPosition = vec4(0);
+    vec3 animatedNormal = vec3(0);
+    for (int i = 0 ; i < 4; i++)
+    {
+        int index = int(joints[i]);
+        float weight = weights[i];
+
+        if (index == -1 || weight == -1 || index >= 100) continue;
+
+        mat4 matrix = jointMatrices[index];
+        vec4 local = matrix * vec4(aPosition, 1.0);
+        animatedPosition += local * weight;
+        animatedNormal = mat3(matrix) * aNormal;
+    }
+
     uv = aUv;
-    normal = normalize(mat3(transpose(inverse(model))) * aNormal);
-    fragpos = vec3(model * vec4(aPosition, 1.0));
+    normal = normalize(mat3(transpose(inverse(model))) * animatedNormal);
+    fragpos = vec3(model * animatedPosition);
     viewpos = vec3(inverse(view)[3]);
-    gl_Position = proj * view * model * vec4(aPosition, 1.0);
+    gl_Position = proj * view * model * animatedPosition;
 }
